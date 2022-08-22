a7ba065cfce13de09c05be0153ef7039b8eb07aa
zhuxuechang <1494135711@qq.com>
2022-08-22 16:52:53 +0800

    feat: Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { useRoutes } from 'react-router-dom'
 import routes from '~react-pages'
 import './App.css'
 
 function App() {
   return (
-    <>
+    <Suspense fallback={<p>Loading...</p>}>
       {useRoutes(routes)}
-    </>
+    </Suspense>
   )
 }
 
